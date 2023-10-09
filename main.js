b88d924a68f67be9ec0286a255ef3ed8ea0e5976
iskandarusmanov <usman29104@gmail.com>
2023-10-09 18:56:24 +0500

    done

---
+++ main.js
@@ -0,0 +1,591 @@
+// Boolean 1 //
+
+// var A = parseInt(prompt(Enter the number: ))
+
+// if (A > 0) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 2 //
+
+// var A = parseInt(prompt(Enter the number: ))
+
+// if (A % 2 == 1) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 3 //
+
+// var A = parseInt(prompt(Enter the number: ))
+
+// if (A % 2 == 0) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 4 //
+
+// var A = parseInt(prompt(Enter the number: ))
+// var B = parseInt(prompt(Enter the number: ))
+
+// if (A > 2 && B <= 3) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 5 //
+
+// var A = parseInt(prompt(Enter the number: ))
+// var B = parseInt(prompt(Enter the number: ))
+
+// if (A >= 0  B < -2) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 6 //
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+// var C = parseInt(prompt(`Enter the number: `))
+
+// if (A <= B <= C) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 7 //
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+// var C = parseInt(prompt(`Enter the number: `))
+
+// if (A > B && B > C) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 8 //
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+
+// if (A % 2 == 1 && B % 2 == 1) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 9 //
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+
+// if (A % 2 == 1  B % 2 == 1) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 10 //
+
+// var A = parseInt(prompt(Enter the number: ))
+// var B = parseInt(prompt(Enter the number: ))
+
+// if (A % 2 == 1 && B % 2 == 0  A % 2 == 0 && B % 2 == 1) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 11 //
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+
+// if (A % 2 == 1 && B % 2 == 1  A % 2 == 0 && B % 2 == 0) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 12 //
+
+// var A = parseInt(prompt(Enter the number: ))
+// var B = parseInt(prompt(Enter the number: ))
+// var C = parseInt(prompt(Enter the number: ))
+
+// if (A > 0 && B > 0 && C > 0) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 13 //
+
+// var A = parseInt(prompt(Enter the number: ))
+// var B = parseInt(prompt(Enter the number: ))
+// var C = parseInt(prompt(Enter the number: ))
+
+// if (A > 0  B > 0  C > 0) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 14 //
+
+// var A = parseInt(prompt(Enter the number: ))
+// var B = parseInt(prompt(Enter the number: ))
+// var C = parseInt(prompt(Enter the number: ))
+
+// if (A > 0 && B < 0 && C < 0  A < 0 && B > 0 && C < 0  A < 0 && B < 0 && C > 0) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 15 //
+
+// var A = parseInt(prompt(Enter the number: ))
+// var B = parseInt(prompt(Enter the number: ))
+// var C = parseInt(prompt(Enter the number: ))
+
+// if (A > 0 && B > 0 && C < 0  A < 0 && B > 0 && C > 0  A > 0 && B < 0 && C > 0) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 16 //
+
+// var A = parseInt(prompt(Enter the number: ))
+
+// if (100 > A && A > 9 && A % 2 == 0) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 17 //
+
+// var A = parseInt(prompt(Enter the number: ))
+
+// if (1000 > A && A > 99 && A % 2 == 1) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 18 //
+
+// var A = parseInt(prompt(Enter the number: ))
+// var B = parseInt(prompt(Enter the number: ))
+// var C = parseInt(prompt(Enter the number: ))
+
+// if (A === B  B === C  C === A) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 19 //
+
+
+
+
+// var A = parseInt(prompt(Enter the number: ))
+// var B = parseInt(prompt(Enter the number: ))
+// var C = parseInt(prompt(Enter the number: ))
+
+// if ((A % 2 === 0 && (A === -B  A === -C)) 
+//       (B % 2 === 0 && (B === -A  B === -C)) 
+//       (C % 2 === 0 && (C === -A  C === -B))) {
+//         alert(true);
+// } else {
+//     alert(false);
+// }
+      
+
+
+// Boolean 20 //
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+// var C = parseInt(prompt(`Enter the number: `))
+
+// if (A !== C && B !== A && C !== B) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 21 //
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+// var C = parseInt(prompt(`Enter the number: `))
+
+// if (A < B && B < C) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 22 //
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+// var C = parseInt(prompt(`Enter the number: `))
+
+// if (A < B && B < C  A > B && B > C) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean 23 //
+
+// var A = parseInt(prompt(Enter the number: ))
+// var B = parseInt(prompt(Enter the number: ))
+// var C = parseInt(prompt(Enter the number: ))
+
+// if (A === C && C === A) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+
+// If 1 // 
+
+// var A = parseInt(prompt(Enter the number: ))
+
+// if (A >= 1) {
+//     alert(A + 1);
+// } else {
+//     alert(A);
+// }
+
+
+
+// If 2 //
+
+// var A = parseInt(prompt(Enter the number: ))
+
+// if (A >= 1) {
+//     alert(A + 1);
+// } else {
+//     alert(A - 2);
+// }
+
+
+
+// If 3 //
+
+// var A = parseInt(prompt(Enter the number: ))
+
+// if (A >= 1) {
+//     alert(A + 1);
+// } else if (A === 0) {
+//     alert(10); 
+// } else {
+//     alert(A - 2);
+// }
+
+
+
+// If 4 //
+
+// var A = parseInt(prompt(Enter the number: ))
+// var B = parseInt(prompt(Enter the number: ))
+// var C = parseInt(prompt(Enter the number: ))
+
+// var Positive = 0; 
+
+// if (A >= 1) {
+//     Positive++;
+// }
+// if (B >= 1) {
+//     Positive++;
+// }
+// if (C >= 1) {
+//     Positive++;
+// }
+
+// if (Positive > 0) {
+//     alert(Positive numbers: ${Positive});
+// } else {
+//     alert(No positive numbers were entered.);
+// }
+
+
+
+// If 5 //
+
+// var A = parseInt(prompt(Enter the number: ))
+// var B = parseInt(prompt(Enter the number: ))
+// var C = parseInt(prompt(Enter the number: ))
+
+// var Positive = 0; // Initialize a counter for positive numbers
+// var Negative = 0; // Initialize a counter for negative numbers
+
+// if (A > 0) {
+//     Positive++;
+// } else if (A < 0) {
+//     Negative++;
+// }
+
+// if (B > 0) {
+//     Positive++;
+// } else if (B < 0) {
+//     Negative++;
+// }
+
+// if (C > 0) {
+//     Positive++;
+// } else if (C < 0) {
+//     Negative++;
+// }
+
+// if (Positive > 0  Negative > 0) {
+//     if (Positive > 0) {
+//         alert(`Positive numbers: ${Positive}`);
+//     }
+//     if (Negative > 0) {
+//         alert(`Negative numbers: ${Negative}`);
+//     }
+// }
+
+
+
+// If 6 // 
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+
+// if (A > B) {
+//     alert(`A is bigger`);
+// } else {
+//     alert(`B is bigger`);
+// }
+
+
+
+// If 7 //
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+
+// if (B > A) {
+//     alert(`A is smaller`);
+// } else {
+//     alert(`B is smaller`);
+// }
+
+
+
+// If 8 // 
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+
+// if (B > A) {
+//     alert(`${B} is bigger`);
+//     alert(`${A} is smaller`);
+// } else if (A > B) {
+//     alert(`${A} is bigger`);
+//     alert(`${B} is smaller`);
+// } else {
+//     alert("A and B are equal");
+// }
+
+
+
+// If 9 //
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+
+// if (A > B) {
+//     [A, B] = [B, A];
+//     alert("Swapping values: A is now the smaller number, and B is the bigger number.");
+//   } else {
+//     alert("No need to swap values: A is already the smaller number, and B is the bigger number.");
+//   }
+
+
+
+// If 10 // 
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+
+// var C = A + B;
+
+// if (A !== B) {
+//     alert(`${C}`);
+// } else {
+//     alert(0);
+// }
+
+
+
+// If 11 //
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+
+// if (A !== B) {
+//     if (A > B) { 
+//         alert(A);
+//     } else {
+//         alert(B);
+//     }
+// } else {
+//     alert(0);
+// }
+ 
+
+
+// If 12 //
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+// var C = parseInt(prompt(`Enter the number: `))
+
+// if (A <= B && A <= C) {
+//     alert(`The smallest number is: ${A}`);
+//   } else if (B <= A && B <= C) {
+//     alert(`The smallest number is: ${B}`);
+//   } else {
+//     alert(`The smallest number is: ${C}`);
+//   }
+
+
+
+// If 13 //
+
+// var A = parseInt(prompt(`Enter the number: `))
+// var B = parseInt(prompt(`Enter the number: `))
+// var C = parseInt(prompt(`Enter the number: `))
+
+// if (A >= B && A <= C  A <= B && A >= C) {
+//     alert(The central number is: ${A});
+// } else if (B >= A && B <= C || B <= A && B >= C) {
+//     alert(The central number is: ${B});
+// } else {
+//     alert(The central number is: ${C});
+// }
+
+
+
+// If 14 //
+
+// var A = parseInt(prompt(Enter the number: ))
+// var B = parseInt(prompt(Enter the number: ))
+// var C = parseInt(prompt(Enter the number: ))
+
+// var smallest, biggest;
+
+// if (A <= B && A <= C) {
+//   smallest = A;
+// } else if (B <= A && B <= C) {
+//   smallest = B;
+// } else {
+//   smallest = C;
+// }
+
+// if (A >= B && A >= C) {
+//   biggest = A;
+// } else if (B >= A && B >= C) {
+//   biggest = B;
+// } else {
+//   biggest = C;
+// }
+
+// alert(Smallest number: ${smallest});
+// alert(Biggest number: ${biggest});
+
+
+
+// If 15 //
+
+// var A = parseInt(prompt(`Enter the number:` ))
+// var B = parseInt(prompt(`Enter the number: `))
+// var C = parseInt(prompt(`Enter the number:`))
+
+// var largestSum;
+// var firstNumber;
+// var secondNumber;
+
+// if (A + B > B + C && A + B > A + C) {
+//   largestSum = A + B;
+//   firstNumber = A;
+//   secondNumber = B;
+// } else if (B + C > A + B && B + C > A + C) {
+//   largestSum = B + C;
+//   firstNumber = B;
+//   secondNumber = C;
+// } else {
+//   largestSum = A + C;
+//   firstNumber = A;
+//   secondNumber = C;
+// }
+
+// var R = firstNumber + secondNumber;
+
+// alert(`The result: ${R}`);
